a140b8fe10af9f2b31e6f5008a23be43ad519c0c
Sean Fuller <sean.e.fuller@gmail.com>
2013-05-25 15:43:21 -0700

    Crazy Egg analytics part 2

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,3 +2,4 @@
     <strong>Follow Us:</strong><a href="http://www.facebook.com/TeleworkPortfolio" target="_blank">Facebook</a><a href="http://www.linkedin.com/company/telework-portfolio" target="_blank">Linkedin</a> <a href="https://plus.google.com/104387030547225838771" target="_blank">Google+</a> <a href="http://teleworkportfolio.blogspot.com" target="_blank">Blogs</a><!-- <a href="#">Youtube</a>--><br />Copyright &copy; 2013 Telework Portfolio, All rights reserved</div>
 </div>
 <?php  $this->load->view('googletracking'); ?>
+<?php  $this->load->view('crazyegg'); ?>
